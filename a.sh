a823ec826983b1b47f6ff90e3fae7b5376911d7a
Benjamin Voca <benjaminvocaa@gmail.com>
2025-07-04 17:22:28 +0200

    m

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
 !#/usr/bin/env bash
+alias nix="nix --extra-experimental-features flakes --extra-experimental-features nix-command"
 nix build .#nixosConfigurations.ci-host.config.system.build.toplevel
 
