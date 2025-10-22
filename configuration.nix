bc0eea6184792bada9e39bf3509131bad2e9a97f
Benjamin Voca <Benjamin-MacBook-Pro.local>
2025-10-22 10:31:30 +0200

    fix: add extra mongo ports to firewall

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,7 @@ in
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 22 80 27017 5555 ];
+    allowedTCPPorts = [ 22 80 27017 27018 27019 27020 5555 ];
     # allowedTCPPortRanges = [
     #   { from = 5100; to = 8999; }
     # ];
